cc4f903627eafbf9113a57650f2e734f516e3abe
venky2809 <jvenkatesh494@gmail.com>
2025-11-08 17:24:14 +0000

    Update AMI and key name for AWS instance

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0a0ff88d0f3f85a14"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "maluma"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
